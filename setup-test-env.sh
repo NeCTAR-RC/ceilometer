2c3ddc82b18c983687a88cca5ff4775f1f98f6cb
Sam Morrison <sorrison@gmail.com>
2015-03-13 11:21:55 +1100

    Skip DB tests
    Change-Id: I011dba4cbf86881d8c899d98f21daca99d21ab74

--- setup-test-env.sh
+++ setup-test-env.sh
@@ -13,25 +13,25 @@ function clean_exit(){
 }
 
 # Setup MongoDB test server
-MONGO_DATA=`mktemp -d CEILO-MONGODB-XXXXX`
-MONGO_PORT=29000
-trap "clean_exit" EXIT
-mkfifo ${MONGO_DATA}/out
-export PATH=${PATH:+$PATH:}/sbin:/usr/sbin
-if ! which mongod >/dev/null 2>&1
-then
-    echo "Could not find mongod command" 1>&2
-    exit 1
-fi
-mongod --maxConns 32 --nojournal --noprealloc --smallfiles --quiet --noauth --port ${MONGO_PORT} --dbpath "${MONGO_DATA}" --bind_ip localhost --config /dev/null &>${MONGO_DATA}/out &
+#MONGO_DATA=`mktemp -d CEILO-MONGODB-XXXXX`
+#MONGO_PORT=29000
+#trap "clean_exit" EXIT
+#mkfifo ${MONGO_DATA}/out
+#export PATH=${PATH:+$PATH:}/sbin:/usr/sbin
+#if ! which mongod >/dev/null 2>&1
+#then
+#    echo "Could not find mongod command" 1>&2
+#    exit 1
+#fi
+#mongod --maxConns 32 --nojournal --noprealloc --smallfiles --quiet --noauth --port ${MONGO_PORT} --dbpath "${MONGO_DATA}" --bind_ip localhost --config /dev/null &>${MONGO_DATA}/out &
 # Wait for Mongo to start listening to connections
-while read line
-do
-    echo "$line" | grep -q "waiting for connections on port ${MONGO_PORT}" && break
-done < ${MONGO_DATA}/out
+#while read line
+#do
+#    echo "$line" | grep -q "waiting for connections on port ${MONGO_PORT}" && break
+#done < ${MONGO_DATA}/out
 # Read the fifo for ever otherwise mongod would block
-cat ${MONGO_DATA}/out > /dev/null &
-export CEILOMETER_TEST_MONGODB_URL="mongodb://localhost:${MONGO_PORT}/ceilometer"
+#cat ${MONGO_DATA}/out > /dev/null &
+#export CEILOMETER_TEST_MONGODB_URL="mongodb://localhost:${MONGO_PORT}/ceilometer"
 if test -n "$CEILOMETER_TEST_HBASE_URL"
 then
     export CEILOMETER_TEST_HBASE_TABLE_PREFIX=$(hexdump -n 16 -v -e '/1 "%02X"' /dev/urandom)
