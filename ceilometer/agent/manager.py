b9bbc9feea0bb4d7fcf133fd18ab53da24164727
Sam Morrison <sorrison@gmail.com>
2018-05-10 10:27:49 +1000

    Ability to define batch size off polled samples.
    Currently if batch_polled_samples=True (default) then it will batch up
    all samples of a poller into one message. This can be problematic if
    the poller returns a large number of samples.
    
    Change-Id: Ic325cd07a8cb83228cf42fb3646579997cf56f6b

--- ceilometer/agent/manager.py
+++ ceilometer/agent/manager.py
@@ -50,6 +50,9 @@ OPTS = [
                 help='To reduce polling agent load, samples are sent to the '
                      'notification agent in a batch. To gain higher '
                      'throughput at the cost of load set this to False.'),
+    cfg.IntOpt('polling_batch_size',
+               default=50,
+               help='Size to batch polled samples',),
     cfg.IntOpt('shuffle_time_before_polling_task',
                default=0,
                help='To reduce large requests at same time to Nova or other '
@@ -139,6 +142,7 @@ class PollingTask(object):
         self.resources = collections.defaultdict(resource_factory)
 
         self._batch = self.manager.conf.batch_polled_samples
+        self._batch_size = self.manager.conf.polling_batch_size
         self._telemetry_secret = self.manager.conf.publisher.telemetry_secret
 
     def add(self, pollster, source):
@@ -201,6 +205,9 @@ class PollingTask(object):
                                 sample, self._telemetry_secret
                             ))
                         if self._batch:
+                            if len(sample_batch) > self._batch_size:
+                                self._send_notification(sample_batch)
+                                sample_batch = []
                             sample_batch.append(sample_dict)
                         else:
                             self._send_notification([sample_dict])
